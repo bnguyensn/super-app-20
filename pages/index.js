e87d5f71ec5a99c1933e79ba484d94318f8f6bf1
Binh Nguyen <b.nguyen.sn@gmail.com>
2020-12-29 16:46:04 +0000

    update app

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import Head from 'next/head';
 import createBTree from '../libs/createBTree';
 
 function generateNodeEl(node) {
@@ -18,7 +19,7 @@ function generateNodeEl(node) {
 export default function IndexPage() {
   const treeRef = useRef(null);
   const [treeJson, setTreeJson] = useState();
-  const [treeHeight, setTreeHeight] = useState(1);
+  const [treeHeight, setTreeHeight] = useState(3);
 
   const onHeightChange = (e) => {
     const newHeight = e.target.value;
@@ -48,28 +49,88 @@ export default function IndexPage() {
     treeEl = <div>{generateNodeEl(treeJson)}</div>;
   }
 
+  useEffect(() => {
+    if (treeHeight && !treeJson) {
+      refreshTree();
+    }
+  }, [treeHeight, treeJson, refreshTree]);
+
   return (
     <div>
-      <main>
+      <Head>
+        <title>Invert binary tree</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="What is a binary tree and how to invert one"
+        />
+        <meta
+          name="keywords"
+          content="bnguyensn, binary tree, invert binary tree"
+        />
+      </Head>
+      <main className="w-screen px-2 overflow-x-hidden">
         <h1 className="text-4xl text-center my-4">Invert Binary Tree</h1>
-        <div className="flex justify-center items-center p-2">
-          <label htmlFor="tree-height-input">Tree height:</label>
-          <input
-            id="tree-height-input"
-            className="mx-2 p-2 w-20"
-            type="number"
-            min={1}
-            value={Number(treeHeight)}
-            onChange={onHeightChange}
-          />
-          <button className="btn-main mx-2" onClick={refreshTree}>
-            Generate
-          </button>
-          <button className="btn-main mx-2" onClick={invertTree}>
-            Invert
-          </button>
+
+        <div className="flex flex-col sm:flex-row justify-center items-center px-2">
+          <div className="flex items-center mb-2 sm:mb-0">
+            <label htmlFor="tree-height-input">Tree height:</label>
+            <input
+              id="tree-height-input"
+              className="mx-2 p-2 w-20"
+              type="number"
+              min={1}
+              value={Number(treeHeight)}
+              onChange={onHeightChange}
+            />
+          </div>
+          <div className="flex items-center">
+            <button className="btn-main mx-2" onClick={refreshTree}>
+              Generate
+            </button>
+            <button className="btn-main mx-2" onClick={invertTree}>
+              Invert
+            </button>
+          </div>
         </div>
-        <div>{treeEl}</div>
+
+        <div className="my-4 overflow-auto">{treeEl}</div>
+
+        <div className="flex flex-col items-center p-2 bg-blue-50">
+          <p className="paragraph">
+            A <b>binary tree</b> is a type of data structure. It&#39;s like a
+            tree with many branches. Each branch can have either 0, 1, or 2
+            child branches.
+          </p>
+
+          <p className="paragraph">
+            Inverting a binary tree means making the left child branch the right
+            child branch, and the right child branch the left child branch,
+            across the whole tree.
+          </p>
+
+          <p className="paragraph">
+            Made by{' '}
+            <a
+              className="link"
+              href="https://twitter.com/bnguyensn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @bnguyensn
+            </a>
+            . Source code on{' '}
+            <a
+              className="link"
+              href="https://github.com/bnguyensn/super-app-20"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+            .
+          </p>
+        </div>
       </main>
     </div>
   );
